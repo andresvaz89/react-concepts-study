4bbb3a6dee388f427ea556744df9b2849d12a14f
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-01-20 12:38:43 +0100

    improved folder and css structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import CoreConcept from './components/CoreConcepts';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcepts/CoreConcepts';
 
 import { CORE_CONCEPTS } from './data';
 
